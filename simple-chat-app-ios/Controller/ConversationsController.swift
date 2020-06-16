d34aa85c7bf89523a1dbcf23d833221bf3f28f06
masterambi <ramzyrash@gmail.com>
2020-06-16 13:02:39 +0700

    Initial project setup

---
+++ simple-chat-app-ios/Controller/ConversationsController.swift
@@ -0,0 +1,40 @@
+//
+//  ConversationsController.swift
+//  simple-chat-app-ios
+//
+//  Created by Ramzy Rashaun Arief on 16/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showProfile() {
+        print(123)
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+}
---
+++ simple-chat-app-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-chat-app-ios
+//
+//  Created by Ramzy Rashaun Arief on 16/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
