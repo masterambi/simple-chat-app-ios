1a9cc2dcd6ae4b86043e51d145a1a044f128bf50
masterambi <ramzyrash@gmail.com>
2020-06-18 18:22:24 +0700

    Refactor login and registration firebase api

--- simple-chat-app-ios/Controller/Authentication/RegistrationController.swift
+++ simple-chat-app-ios/Controller/Authentication/RegistrationController.swift
@@ -94,46 +94,17 @@ class RegistrationController: UIViewController {
         guard let username = usernameTextField.text?.lowercased() else { return }
         guard let profileImage = profileImage else { return }
         
-        guard let imageData = profileImage.jpegData(compressionQuality: 0.2) else { return }
+        let credentials = RegistrationCredentials(email: email, password: password,
+                                                  fullname: fullname, username: username,
+                                                  profileImage: profileImage)
         
-        let filename = NSUUID().uuidString
-        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
-        
-        ref.putData(imageData, metadata: nil) { (meta, error) in
+        AuthService.shared.createUser(credentials: credentials) { error in
             if let error = error {
-                print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
+                print("DEBUG: \(error.localizedDescription)")
                 return
             }
-            
-            ref.downloadURL { (url, error) in
-                guard let profileImageUrl = url?.absoluteString else { return }
-                
-                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-                    if let error = error {
-                        print("DEBUG: Failed to create user with error: \(error.localizedDescription)")
-                        return
-                    }
-                    
-                    guard let uid = result?.user.uid else { return }
-                    
-                    let data = ["email": email,
-                                "fullname": fullname,
-                                "profileImageUrl": profileImageUrl,
-                                "uid": uid,
-                                "username": username] as [String: Any]
-                    
-                    Firestore.firestore().collection("users").document(uid).setData(data) { error in
-                        if let error = error {
-                            print("DEBUG: Failed to upload user data with error: \(error.localizedDescription)")
-                            return
-                        }
-                        
-                        self.dismiss(animated: true, completion: nil)
-                    }
-                }
-            }
+            self.dismiss(animated: true, completion: nil)
         }
-        
     }
     
     @objc func textDidChange(sender: UITextField) {
