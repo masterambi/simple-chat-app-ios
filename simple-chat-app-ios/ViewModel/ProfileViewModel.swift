852d7bf6230e193ce9fe60d4dfe5c7cebe4891e1
masterambi <ramzyrash@gmail.com>
2020-06-22 15:06:08 +0700

    Add profile view model for profile cell, setup profile cell UI

---
+++ simple-chat-app-ios/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewModel.swift
+//  simple-chat-app-ios
+//
+//  Created by Ramzy Rashaun Arief on 22/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import Foundation
+
+enum ProfileViewModel: Int, CaseIterable {
+    case accountInfo
+    
+    case settings
+    
+    var description: String {
+        switch self {
+        case .accountInfo: return "Account Info"
+        case . settings: return "Settings"
+        }
+    }
+    
+    var iconImageName: String {
+        switch self {
+        case .accountInfo: return "person.circle"
+        case .settings: return "gear"
+        }
+    }
+}
